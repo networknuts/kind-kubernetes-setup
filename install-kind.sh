48e2cb233039e8faba67fc0022fac0b611ca8189
arytmw <54942957+arytmw@users.noreply.github.com>
2025-12-09 19:00:47 +0530

    Create install-kind.sh

---
+++ install-kind.sh
@@ -0,0 +1,4 @@
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+kind create cluster --name k8s.networknuts.lab --config .cluster.yaml
