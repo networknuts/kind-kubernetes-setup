18023b5de5912c1b7019b598f6c2d142aa3b6bd7
arytmw <54942957+arytmw@users.noreply.github.com>
2026-02-22 09:23:29 +0530

    Update install-kubectl.sh

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -1,6 +1,5 @@
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv kubectl /usr/bin
-sudo apt-get install -y bash-completion
-echo "source <(kubectl completion bash)" >> ~/.bashrc
-source ~/.bashrc
+kind export kubeconfig --name k8s.networknuts.lab --kubeconfig /home/ubuntu/.kube/config
+kubectl get nodes
